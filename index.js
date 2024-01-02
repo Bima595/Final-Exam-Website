b19b6781b9ba6b5b4356ce504228e58196423770
Bima595 <satriaabimanyu595@gmail.com>
2024-01-02 13:36:43 +0700

    test

---
+++ index.js
@@ -0,0 +1,18 @@
+// var cd1 = document.getElementById("cd1");
+// var cd2 = document.getElementById("cd2");
+// var cd3 = document.getElementById("cd3");
+// var pil1 = document.getElementById("pl1");
+// var pil2 = document.getElementById("pl2");
+// var cash = document.getElementById("cash");
+// var display = document.getElementById("display");
+
+function show(){
+    var cd1 = document.getElementById("cd1").value = 1 ;
+    var cd2 = document.getElementById("cd2").value = 2;
+    var cd3 = document.getElementById("cd3").value = 3;
+    var pil1 = document.getElementById("pl1").value= ;
+    var hg1 = document.getElementById("harga1").value= ;
+    var hg2 = document.getElementById("harg2").value = ;
+    var hg1 = document.getElementById("harga3").value= ;
+
+}
